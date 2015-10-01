700cee13769ffb27749674534fcb0c50e0dfaef4
efeld016 <efeld016@fiu.edu>
2015-09-30 22:13:34 -0400

    fix menu bug and working on gps, there is still kind of lagging/baging with the gps so need to fix that

--- Code/voterCloud/client/client.js
+++ Code/voterCloud/client/client.js
@@ -1,6 +1,10 @@
 Session.setDefault('image', 1);
 Session.setDefault('jason', null);
 
+Session.setDefault('lat', undefined);
+Session.setDefault('lon', undefined);
+Session.setDefault('ad', undefined);
+
 Router.configure({
   layoutTemplate: "layout",
   loadingTemplate: 'loading',
@@ -13,6 +17,16 @@ Router.route('/', function () {
 
 Router.route('/Search', function () {
   this.render('Search');
+});
+
+Meteor.startup(function() {
+    if (Session.get('lat') == undefined 
+             || Session.get('lon') == undefined) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            Session.set('lat', position.coords.latitude);
+            Session.set('lon', position.coords.longitude);
+        });
+    }
 });
 
 Template.layout.events({
@@ -50,130 +64,168 @@ Template.layout.events({
 
 	}
 });
-Template.Search.events({
-	'submit #Search': function(event){
-		event.preventDefault();
-		var address = event.target.address.value;
-		console.log(address);
-		
-		Meteor.call('civicAddress', address,  function (error, result) {
-			if(error) {
-				console.log("error occured on receiving data on server. ", error );
-			} else {
 
-				var arr = [ ];
+function gtthat()
+{
+	var addresss="";
+	Meteor.call('address', Session.get( 'lat' ), Session.get( 'lon' ),  function (error, result) {
+		if(error) {
+			console.log("error occured on receiving data on server. ", error );
+		}
+		else {
+			addresss = result.results[0].formatted_address;
+			Session.set('ad', addresss);
+			console.log("Printing");
+			console.log(addresss);
+		}
+	});
 
-				for( var prop2 in result.divisions )
+}
+
+function submiting()
+{
+	console.log( "calling submit" );
+	var address;
+	if ( Session.get( 'ad' ) != undefined ) {
+
+		address = Session.get('ad');
+	}	
+	else {
+		address = event.target.address.value;
+	}
+	Session.set('ad', undefined);
+
+	Meteor.call('civicAddress', address,  function (error, result) {
+		if(error) {
+			console.log("error occured on receiving data on server. ", error );
+		} else {
+
+			var arr = [ ];
+
+			for( var prop2 in result.divisions )
+			{
+				if( result.divisions.hasOwnProperty( prop2 ) )
 				{
-					if( result.divisions.hasOwnProperty( prop2 ) )
-					{
-						var prop = result.divisions[ prop2 ];
-						var tag = prop2;
-						var division = prop.name;
-						if( prop.hasOwnProperty( 'officeIndices' ) ){
+					var prop = result.divisions[ prop2 ];
+					var tag = prop2;
+					var division = prop.name;
+					if( prop.hasOwnProperty( 'officeIndices' ) ){
 
-							for ( var i = 0; i < prop.officeIndices.length; i++ ) {
+						for ( var i = 0; i < prop.officeIndices.length; i++ ) {
 
-								var office = result.offices[ prop.officeIndices[ i ] ];
-								var hasLevel = office.hasOwnProperty( 'levels' );
-								var level = ( hasLevel )? office.levels[ 0 ] : "";
-								var hasOfficeName = office.hasOwnProperty( 'name' );
-								var officeName = ( hasOfficeName )? office.name : "";
-								var hasRole = office.hasOwnProperty( 'roles' );
-								var role = ( hasRole )? office.roles[ 0 ] : "";
+							var office = result.offices[ prop.officeIndices[ i ] ];
+							var hasLevel = office.hasOwnProperty( 'levels' );
+							var level = ( hasLevel )? office.levels[ 0 ] : "";
+							var hasOfficeName = office.hasOwnProperty( 'name' );
+							var officeName = ( hasOfficeName )? office.name : "";
+							var hasRole = office.hasOwnProperty( 'roles' );
+							var role = ( hasRole )? office.roles[ 0 ] : "";
 
-								for ( var j = 0; j < office.officialIndices.length; j++ ) {
+							for ( var j = 0; j < office.officialIndices.length; j++ ) {
 
-									var official = result.officials[ office.officialIndices[ j ] ];
-									var officialName = official.name;
-									var hasParty = official.hasOwnProperty( 'party' );
-									var party = ( hasParty )? official.party : "";
-									var partyImg = ( hasParty && party != "Unknown" )? "/" + party + ".png" : "/blank.gif";
-									var hasPhone = official.hasOwnProperty( 'phones' );
-									var phone = ( hasPhone )? official.phones[ 0 ] : "";
-									var hasPhoto = official.hasOwnProperty( 'photoUrl' );
-									var photo = ( hasPhoto )? official.photoUrl : "";
-									var hasFacebook = false;
-									var facebook;
-									var hasYoutube = false;
-									var youtube;
-									var hasTwitter = false;
-									var twitter;
+								var official = result.officials[ office.officialIndices[ j ] ];
+								var officialName = official.name;
+								var hasParty = official.hasOwnProperty( 'party' );
+								var party = ( hasParty )? official.party : "";
+								var partyImg = ( hasParty && party != "Unknown" )? "/" + party + ".png" : "/blank.gif";
+								var hasPhone = official.hasOwnProperty( 'phones' );
+								var phone = ( hasPhone )? official.phones[ 0 ] : "";
+								var hasPhoto = official.hasOwnProperty( 'photoUrl' );
+								var photo = ( hasPhoto )? official.photoUrl : "";
+								var hasFacebook = false;
+								var facebook;
+								var hasYoutube = false;
+								var youtube;
+								var hasTwitter = false;
+								var twitter;
 
-									if( official.hasOwnProperty( 'channels' ) ) {
+								if( official.hasOwnProperty( 'channels' ) ) {
 
-										for ( var h = 0; h < official.channels.length; h++ ) {	
-											switch ( official.channels[ h ].type )
-											{
-												case "Twitter":
-													twitter = "https://twitter.com/"+official.channels[ h ].id;
-													hasTwitter = true;
-													break;
-												case "Facebook":
-													facebook = "https://www.facebook.com/"+official.channels[ h ].id;
-													hasFacebook = true;
-													break;
-												case "YouTube":
-													youtube = "https://www.youtube.com/user/"+official.channels[ h ].id;
-													hasYoutube = true;
-													break;
-											}
+									for ( var h = 0; h < official.channels.length; h++ ) {	
+										switch ( official.channels[ h ].type )
+										{
+											case "Twitter":
+												twitter = "https://twitter.com/"+official.channels[ h ].id;
+												hasTwitter = true;
+												break;
+											case "Facebook":
+												facebook = "https://www.facebook.com/"+official.channels[ h ].id;
+												hasFacebook = true;
+												break;
+											case "YouTube":
+												youtube = "https://www.youtube.com/user/"+official.channels[ h ].id;
+												hasYoutube = true;
+												break;
 										}
 									}
-									/**
-									* ALL THE PROPERTIES OF THE OBJECT DESCRIBED HERE !! FOR EACH OFFICIAL !!
-									*/
-									var obg = {
-										division: division,
-										hasLevel: hasLevel,
-										level: level,
-										hasOfficeName: hasOfficeName,
-										officeName: officeName,
-										hasRole: hasRole,
-										role: role,
-										officialName: officialName,
-										hasParty: hasParty,
-										party: party,
-										partyImg: partyImg,
-										hasPhone: hasPhone,
-										phone: phone,
-										hasPhoto: hasPhoto,
-										photo: photo,
-										tag: tag,
-										hasFacebook: hasFacebook,
-										facebook: facebook,
-										hasYoutube: hasYoutube,
-										youtube: youtube,
-										hasTwitter: hasTwitter,
-										twitter: twitter
-									};
-
-									arr.push( obg );
-
 								}
+								/**
+								* ALL THE PROPERTIES OF THE OBJECT DESCRIBED HERE !! FOR EACH OFFICIAL !!
+								*/
+								var obg = {
+									division: division,
+									hasLevel: hasLevel,
+									level: level,
+									hasOfficeName: hasOfficeName,
+									officeName: officeName,
+									hasRole: hasRole,
+									role: role,
+									officialName: officialName,
+									hasParty: hasParty,
+									party: party,
+									partyImg: partyImg,
+									hasPhone: hasPhone,
+									phone: phone,
+									hasPhoto: hasPhoto,
+									photo: photo,
+									tag: tag,
+									hasFacebook: hasFacebook,
+									facebook: facebook,
+									hasYoutube: hasYoutube,
+									youtube: youtube,
+									hasTwitter: hasTwitter,
+									twitter: twitter
+								};
+
+								arr.push( obg );
+
 							}
 						}
 					}
 				}
-				function compare(a, b)
-				{
-					if( a.tag.length < b.tag.length )
-						return -1;
-					if( a.tag.length > b.tag.length )
-						return 1;
-					return 0;
-				};
-				arr.sort( compare );
-				Session.set( 'jason',arr );
-				console.log( arr );
-				console.log( result );
 			}
-		});
+			function compare(a, b)
+			{
+				if( a.tag.length < b.tag.length )
+					return -1;
+				if( a.tag.length > b.tag.length )
+					return 1;
+				return 0;
+			};
+			arr.sort( compare );
+			Session.set( 'jason',arr );
+			console.log( result );
+		}
+	});
+};
+
+Template.Search.events({
+	'click #but' : function(event){
+		event.preventDefault();
+		gtthat();
+		submiting();
+	},
+	'submit #Search': function(event){
+		event.preventDefault();
+		submiting();
 	}
 });
+
 
 Template.Search.helpers({
 	officials: function(){
 		return Session.get( 'jason' );
 	}
 });
+
+//Geolocation.latLng();
--- Code/voterCloud/server/server.js
+++ Code/voterCloud/server/server.js
@@ -1,8 +1,8 @@
-
+/*
 Meteor.startup(function(){
     
 });
-
+*/
 Meteor.methods({
 	civicAddress: function (address) {
 			/**
@@ -24,6 +24,26 @@ Meteor.methods({
 				throw new Meteor.Error(ret.statusCode, errorJson.error);
 			}
 
-        }
+        },
+    address: function(lat,lon) {
+    		/**
+			*	TYPE YOUR GOOGLE KEY HERE =======================================
+			**/
+			var key = "AIzaSyAdmDs9YqqhHMh0T_Mr_g895KLvUaLvtd4"; /*<------YOUR KEY-------*/
+			/**
+			* YOUR GOOGLE API KEY TYPE HERE =====================================
+			**/
+			var url="https://maps.googleapis.com/maps/api/geocode/json?latlng="+lat+","+lon+"&key="+key;
+			var ret= Meteor.http.call("GET", url);
+			if(ret.statusCode==200) {
+				var respJson = JSON.parse(ret.content);
+				console.log("response received.");
+				return respJson;
+			} else {
+				console.log("Response issue: ", ret.statusCode);
+				var errorJson = JSON.parse(ret.content);
+				throw new Meteor.Error(ret.statusCode, errorJson.error);
+			}
+    }
 });
-/*https://gist.github.com/nachiket-p/2922814#file-app-js-L12*///For beeter exlantion how to make http calls in meteor
+
